8cda9dbe7d84df3a07a734522a91060a37249f1a
theyloveBMNEG7 <tchokoanssiemmanuel@gmail.com>
2025-01-20 04:24:43 +0100

    emmanuel work

---
+++ libs/dbConnect.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+mongoose.connect(MONGODB_URI, {
+    dbName: 'chantier_project',
+    bufferCommands: false,
+});
+
+console.log('Connected to MongoDB');
